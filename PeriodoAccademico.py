97d189cc18d33195f1bcdd466fb6f36eb0de9882
Luis Carrillo Gutiérrez <carrilloluis@users.noreply.github.com>
2026-01-03 20:22:12 -0500

    Add files via upload

---
+++ PeriodoAccademico.py
@@ -0,0 +1,47 @@
+from __main__ import app, DB, MINIMAL_CORS
+import sqlite3
+import json
+import bottle
+from contextlib import closing
+
+@app.route('/v1/PeriodoAccademico/<idx:int>/', method='GET')
+def show_pa(idx:int):
+	ds_ = {}
+	str_show = """
+		SELECT [i] AS id, COALESCE([a], '') AS nm 
+		FROM [t1] 
+		WHERE [i]=?
+		"""
+	try:
+		with sqlite3.connect(DB) as connection_:
+			with closing(connection_.cursor()) as cursor_:
+				cursor_.row_factory = sqlite3.Row
+				cursor_.execute(str_show, (idx, ))
+				ds_ = [dict(r) for r in cursor_.fetchall()]
+	except sqlite3.OperationalError as e1:
+		return bottle.HTTPResponse(body=json.dumps({'error' : str(e1) }), status=500)
+	if len(ds_) > 0:
+		return bottle.HTTPResponse(body=json.dumps(ds_), status=200, headers=MINIMAL_CORS)
+	else:
+		return bottle.HTTPResponse(body={}, status=404)
+
+@app.route('/v1/PeriodoAccademico/__/', method='GET')
+def list_pa():
+	ds_ = {}
+	str_list = """
+		SELECT [i] AS id, COALESCE([a], '') AS nm
+		FROM [t1] 
+		WHERE (([f] & 1) = 1)
+		"""
+	try:
+		with sqlite3.connect(DB) as connection_:
+			with closing(connection_.cursor()) as cursor_:
+				cursor_.row_factory = sqlite3.Row
+				cursor_.execute(str_list)
+				ds_ = [dict(r) for r in cursor_.fetchall()]
+	except sqlite3.OperationalError as e1:
+		return bottle.HTTPResponse(body=json.dumps({'error' : str(e1) }), status=500)
+	if len(ds_) > 0:
+		return bottle.HTTPResponse(body=json.dumps(ds_), status=200, headers=MINIMAL_CORS)
+	else:
+		return bottle.HTTPResponse(body={}, status=404)
